86244ea0e1048c6336c17c57f148587d32d918d8
David Danielson <David@Davids-MacBook.local>
2016-09-20 15:31:22 -0600

    User and Car functions added and updated

--- practice.js
+++ practice.js
@@ -1,11 +1,11 @@
 //We're in a job interview. Answer the following questions (try to not look at your notes unless you have to).
   // 1) What is the purpose of the 'this keyword'?
-      "This" allows you to set the scope of your function. "This" calls upon a specific object within function.
+      // "This" allows you to set the scope of your function. "This" calls upon a specific object within function.
       //Answer
 
   // 2) What are the four rules that govern what the 'this keyword' is bound to and describe each?
-        Explicit:
-        Implicit:
+        // Explicit:
+        // Implicit:
 
       //Answer
 
@@ -41,6 +41,15 @@
 
 
 // Write the function definitions which will make the following function invocations function properly.
+function Car(make, model, year){
+	this.make = make;
+	this.model = model;
+	this.year = year;
+	this.move = 0;
+	this.moveCar = function() {
+		return this.move += 10;
+	}
+}
 
   //Function Invocations Here
 
